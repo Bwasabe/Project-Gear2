a132296324373c980699df3f0b9489bfc580746b
Bwasabe <jixg20@gmail.com>
2023-04-06 22:49:45 +0900

    조이스틱 에셋 임포트

---
+++ Assets/01.Scripts/08.Managers/CharacterManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoSingleton<CharacterManager>
+{
+    [field: SerializeField]
+    public CharacterComponentController Knight{ get; private set; }
+
+    private List<Transform> _characters = new();
+
+    private void Awake()
+    {
+        _characters.Add(Knight.transform);
+    }
+    
+
+    public Transform GetClosestCharacter(Transform enemy)
+    {
+        if(_characters.Count <= 0) return null;
+
+        Transform closestCharacter = null;
+
+        float minSqrMagnitude = float.MaxValue;
+
+        foreach (Transform character in _characters)
+        {
+            if(!character.gameObject.activeSelf) continue;
+            
+            float sqrMagnitude = (character.position - enemy.position).sqrMagnitude;
+            
+            if(sqrMagnitude < minSqrMagnitude)
+            {
+                minSqrMagnitude = sqrMagnitude;
+                closestCharacter = character;
+            }
+        }
+
+        return closestCharacter;
+    }
+}
